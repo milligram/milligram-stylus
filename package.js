4546c11ee9c090758de7d40e9ce880073bbcd399
CJ Patoilo <cjpatoilo@gmail.com>
2020-06-19 01:18:46 -0300

    Build new release v1.4.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'milligram:milligram-stylus',
-  version: '1.3.0',
+  version: '1.4.0',
   summary: 'A minimalist CSS framework on Stylus version.',
   git: 'https://github.com/milligram/milligram-stylus.git',
   documentation: 'readme.md',
